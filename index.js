5a4c7509124a5da64356726acb7781f9e5aecf20
EmiTorres93 <emilianat1992@gmail.com>
2023-09-12 17:48:21 -0300

    agregamos la configuración de los módulos

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+console.log("esto es una prueba2");
+//El archivo ppal del backend lo vamos a dividir en 3 partes
+
+// 1. Configuraciones inciales como el puerto (aquí le pedimos ayuda a Express)
+
+// 2. Vamos a configurar en el medio del proyecto algo que se llama middlewares (son funciones de JS)
+//se llaman así en el backend las funciones que me van a dar una habilidad específica en nuestro backedn por ej que pueda recibir solicitudes externas
+
+// 3. Crear las rutas
